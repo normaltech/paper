f87c1aa57f2ca6806fd9c556a1455f47a7ffd644
KYJ <kim_young_@naver.com>
2021-08-28 17:00:26 +0900

    add foobar

--- src/components/footbar/Footbar.jsx
+++ src/components/footbar/Footbar.jsx
@@ -1,9 +1,36 @@
 import "./footbar.css"
-
+/* eslint-disable-next-line */
 export default function Footbar() {
   return (
-    <div>
-      
-    </div>
+    <footer class= "footer-distributed">
+      <div class= "footer-left">
+
+        <img className="logoImg" src="/assets/Artboard 6_1.png" alt="" />
+       
+        <p class="footer-links">
+					<a class="link-1" href="https://www.naver.com">Home</a>
+					<a href="https://www.naver.com">Approach</a>
+					<a href="https://www.naver.com">About us</a>
+					<a href="https://www.naver.com">FAQ</a>
+					<a href="https://www.naver.com">Contact us</a>
+				</p>
+        
+        <p> SLINK LABS © 2021 </p>
+      </div>
+    
+      <div class="footer-right">
+
+				<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com">
+          <i class="fa fa-facebook"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com">
+          <i class="fab fa-instagram"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.telegram.com">
+          <i class="fab fa-telegram"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com">  
+          <i class="fa fa-github"></i></a>
+
+			</div>
+
+    </footer>
   )
 }
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,10 +1,12 @@
-import "./home.css"
-import Body from "../components/body/Body"
+import "./home.css";
+import Body from "../components/body/Body";
+import Footbar from "../components/footbar/Footbar";
 
 export default function Home() {
   return (
     <>
       <Body />
+      <Footbar />
     </>
   );
 }
