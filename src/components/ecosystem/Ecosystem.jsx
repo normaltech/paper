9ef375b41bc07e6ab825582dfb1212da65ae736f
JH <silkcarp97@gmail.com>
2021-08-30 22:43:00 +0900

    ecosystem fatal error

--- src/components/ecosystem/Ecosystem.jsx
+++ src/components/ecosystem/Ecosystem.jsx
@@ -2,12 +2,34 @@ import "./ecosystem.css"
 
 export default function Ecosystem() {
   return (
+
     <div className="ecosystemContainer">
       <div className="ecoImg">
         <img src="/assets/Website Graphics/Ecosystem Map/map2.jpg" alt="" />
       </div>
       <div className="center_top">
         <img className="img31" src="/assets/Artboard 3.png" alt="" />
+        <div />
+        <div className="ecosystemConatiner">
+          <div className="-\31 ">
+            <img className="img31" src="/assets/Artboard 3.png" alt="" />
+          </div>
+          {/* <div className="-\32 ">
+
+          </div>
+          <div className="-\357">
+
+          </div>
+          <div className="-\35 8">
+
+          </div>
+          <div className="-\35 9">
+
+          </div>
+          <div className="-\36 0">
+
+          </div> */}
+        </div>
       </div>
     </div>
   )
